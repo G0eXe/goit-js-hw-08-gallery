0ff996cf60cd1fc393caf8135184b6861bc92c80
Andrei Dunaievskyi <andrei.dag@gmail.com>
2021-07-12 15:15:12 +0300

    Добавляет решения ДЗ 8

--- app.js
+++ app.js
@@ -63,3 +63,74 @@ const galleryItems = [
         description: 'Lighthouse Coast Sea',
     },
 ];
+
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('.js-lightbox');
+const bigModalImg = document.querySelector('.lightbox__image');
+const closeModalBtn = document.querySelector('[data-action="close-lightbox');
+const overlay = document.querySelector(' .lightbox__overlay');
+let currentIndex;
+
+// TASK 1 Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.
+const galleryMarkup = galleryItems.reduce(
+    (add, { preview, original, description }) => {
+        return (
+            add +
+            `<li class="gallery__item"><a class="gallery__link" href=${original}><img class="gallery__image" src=${preview} data-source=${original} alt='${description}'/></a></li>`
+        );
+    },
+    '',
+);
+
+gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+// TASK 2 Реализация делегирования на галерее ul.js-gallery
+gallery.addEventListener('click', onClick);
+
+function onClick(evt) {
+    const galleryImage = evt.target;
+    evt.preventDefault();
+    if (galleryImage.nodeName !== 'IMG') {
+        return;
+    }
+
+    // TASK 3 Открытие модального окна по клику на элементе галереи.
+    modal.classList.add('is-open');
+
+    // TASK 4 Подмена значения атрибута src элемента img.lightbox__image получение url большого изображения.
+    bigModalImg.src = galleryImage.dataset.source;
+    currentIndex = +galleryImage.dataset.index;
+}
+
+// TASK 5 Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
+closeModalBtn.addEventListener('click', onCloseModal);
+
+function onCloseModal() {
+    modal.classList.remove('is-open');
+
+    // TASK 6 Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+    bigModalImg.src = '';
+    bigModalImg.alt = '';
+    window.removeEventListener('keydown', onEscKeyPress);
+}
+
+// ДОПОЛНИТЕЛЬНО 1. Закрытие модального окна по клику на div.lightbox__overlay.
+overlay.addEventListener('click', onOverlayClick);
+function onOverlayClick() {
+    onCloseModal();
+}
+
+// ДОПОЛНИТЕЛЬНО 2. Закрытие модального окна по нажатию клавиши ESC.
+const escape = document.querySelector('body');
+escape.addEventListener('keydown', onEscKeyPress);
+function onEscKeyPress(event) {
+    const ESC_KEY_CODE = 'Escape';
+    const isEscKey = event.code === ESC_KEY_CODE;
+
+    if (isEscKey) {
+        onCloseModal();
+    }
+}
+
+// ДОПОЛНИТЕЛЬНО 3. Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
+// ¯\_(ツ)_/¯
